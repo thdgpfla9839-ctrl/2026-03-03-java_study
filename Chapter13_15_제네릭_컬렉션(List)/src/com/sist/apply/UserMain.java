81ca09133343c96425117f16f9d6f0345562b45b
thdgpfla9839@gmail.com <sist-19@DESKTOP-Q086RPO>
2026-04-09 17:36:05 +0900

    복습 잘하기

---
+++ Chapter13_15_제네릭_컬렉션(List)/src/com/sist/apply/UserMain.java
@@ -0,0 +1,181 @@
+package com.sist.apply;
+import java.util.*;
+// => 한개로 묶어서 제어 
+// 1 3 4 6 7 1 3  9  10 4 4 5 7 7 9 10 int[] arr
+/*
+ *   class A extends Object
+ *   {
+ *     finalize() clone() toString hashCode()
+ *     public void display()
+ *   }
+ *   
+ *   A o=new A();
+ *   
+ *   class A
+ *   class B extends A
+ *   
+ *   A a=new B()
+ *   
+ *   
+ */
+public class UserMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        /*Scanner scan=new Scanner(System.in);
+        System.out.print("페이지 입력:");
+        int page=scan.nextInt();
+        
+        FoodSystem fs=new FoodSystem();
+        ArrayList<Food> list=fs.food_list(page);
+        
+        for(Food f:list)
+        {
+        	System.out.println(f.getNo()+"."+f.getName());
+        }
+        
+        System.out.println("========================");
+        System.out.print("상세볼 맛집 번호 입력:");
+        int no=scan.nextInt();
+        Food ff=fs.foodDetail(no);
+        System.out.println("====== 맛집 정보 ======");
+        System.out.println("맛집명:"+ff.getName());
+        System.out.println("음식종류:"+ff.getType());
+        System.out.println("주차:"+ff.getParking());
+        System.out.println("영업시간:"+ff.getTime());
+        System.out.println("가격대:"+ff.getPrice());
+        System.out.println("테마:"+ff.getTheme());
+        System.out.println("소개:"+ff.getContent());
+        System.out.println("주소:"+ff.getAddress());
+        System.out.println("전화:"+ff.getPhone());
+        System.out.println("평점:"+ff.getScore());
+        System.out.println("======================");
+        System.out.print("주소 입력:");
+        String addr=scan.next();
+        ArrayList<Food> aList=fs.foodFind(addr);
+        for(Food f:aList)
+        {
+        	System.out.println(f.getNo()+"."
+        			+f.getName()+"("
+        			+f.getAddress()+")");
+        }*/
+		//List list1=new ArrayList();
+		
+		//Object list2=new LinkedList();
+		//Object list3=new Vector();
+		/*list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(1);
+		list.add("홍길동");
+		list.add('A');
+		list.add(10.5);
+		list.add(true);
+		list.add(10.5);
+		list.add(true);*/
+		
+//		System.out.println(list.get(0));
+//		System.out.println(list.get(1));
+//		System.out.println(list.get(2));
+//		System.out.println(list.get(3));
+//		System.out.println(list.get(4));
+//		System.out.println(list.get(5));
+//		System.out.println(list.get(6));
+//		System.out.println(list.get(7));
+//		System.out.println(list.get(9));
+//		System.out.println(list.get(10));
+		
+//		for(int i=0;i<list.size();i++)
+//		{
+//			Object o=list.get(i);
+//			System.out.println(o);
+//		}
+	}
+
+}
